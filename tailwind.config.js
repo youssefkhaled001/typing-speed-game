1bea4f1a9c73158727429654589771531b6927c0
Youssef Khaled <youssefkhaled001@outlook.com>
2023-09-06 01:11:24 +0300

    style: tailwind default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navBarBg: '#2A0B7D',
+        mainBg: '#4311C7',
+        accentBg: '#370EA4',
+        navTextColor: '#5541A0'
+      }
+    },
   },
   plugins: [],
 }
