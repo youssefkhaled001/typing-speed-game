a34bd923c665e4c700ee403342227fe62209599e
Youssef Khaled <youssefkhaled001@outlook.com>
2023-09-06 01:15:02 +0300

    feat: Created Simple Navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import {Logo} from './'
+import { NavLink } from 'react-router-dom'
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className='w-full h-20 bg-navBarBg flex flex-row justify-between items-center text-white p-5'>
+      <Logo/>
+      <div>
+        <NavLink to={'/'} className='mx-5 font-bold text-navTextColor'>
+          Home
+        </NavLink>
+        <NavLink to={'/play'} className='mx-5 font-bold text-navTextColor'>
+          Play
+        </NavLink>
+        <NavLink to={'/scores'} className='mx-5 font-bold text-navTextColor'>
+          Scores
+        </NavLink>
+      </div>
+    </div>
   )
 }
 
