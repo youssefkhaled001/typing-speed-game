1be0d369d357de0076a1a72df05c9bf9a32a4351
Youssef Khaled <youssefkhaled001@outlook.com>
2023-09-06 01:10:48 +0300

    update: Indecies

---
+++ src/Hooks/index.js
@@ -0,0 +1,2 @@
+export {default as useWordsFetch} from './useWordsFetch'
+export {default as useTimer} from './useTimer'
---
+++ src/Reducers/index.js
@@ -0,0 +1,3 @@
+export {default as GameReducer} from './GameReducer'
+export {default as ProgressReducer} from './ProgressReducer'
+export {default as WordsDisplayReducer} from './WordsDisplayReducer'
