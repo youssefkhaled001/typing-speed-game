5fe38cd09489af127b7e2815cb5477773ce86b7e
Youssef Khaled <youssefkhaled001@outlook.com>
2023-09-06 01:19:37 +0300

    feat: Added Logo & Updated Pages Index

---
+++ src/Components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div className='font-extrabold capitalize'>Typing Game</div>
+  )
+}
+
+export default Logo
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,5 +1,5 @@
 
 export {default as HomePage} from './HomePage'
 export {default as PlayPage} from './PlayPage'
-export {default as ResultsPage} from './ResultsPage'
-
+export {default as ResultPage} from './ResultPage'
+export {default as ScoresPage} from './ScoresPage'
