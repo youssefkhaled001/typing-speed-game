bddcedc1af2593419ce6ae84e3be32c376844edb
Youssef Khaled <youssefkhaled001@outlook.com>
2023-09-06 01:17:05 +0300

    feat: Added Loading Icon

---
+++ src/Components/external/LoadingIcon.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './LoadingIcon.css'
+function LoadingIcon() {
+  return (
+    <div className="dot-spinner">
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+    </div>
+  )
+}
+
+export default LoadingIcon
