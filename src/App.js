74b938f98573736470d9ec8624e3a627697cb98f
Youssef Khaled <youssefkhaled001@outlook.com>
2023-09-06 01:15:30 +0300

    feat: Added Pages Routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import { HomePage, PlayPage, ResultsPage } from './Pages';
+import { HomePage, PlayPage, ResultPage, ScoresPage } from './Pages';
+import { NavBar } from './Components';
 import './App.css';
-
+import { GameContextProvider } from './Contexts/GameContext'
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+    <div className='h-screen flex flex-col text-white'>
+    <BrowserRouter>
+      <NavBar/>
+      <div className='bg-mainBg flex-grow flex flex-col p-10'>
         <Routes>
-          <Route path='/' element={HomePage}/>
-          <Route path= '/play' element={PlayPage}/>
-          <Route path= '/result' element={ResultPage}/>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path= '/play' element={<GameContextProvider><PlayPage/></GameContextProvider>}/>
+          <Route path= '/result' element={<ResultPage/>}/>
+          <Route path= '/scores' element={<ScoresPage/>}/>
         </Routes>
-      </BrowserRouter>
+      </div>
+      
+    </BrowserRouter>
     </div>
   );
 }
